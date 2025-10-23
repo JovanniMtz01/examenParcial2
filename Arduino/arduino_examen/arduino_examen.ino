525079d2cf2fe6165aee248b9262ee84c8d30ee3
JovanniMtz01 <jovanni.mtz.93@gmail.com>
2025-10-22 20:42:36 -0600

    se agrega primer codigo de arduino

---
+++ Arduino/arduino_examen/arduino_examen.ino
@@ -0,0 +1,14 @@
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  delay(1000);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+for (int i=0; i <=1023;i++){
+  Serial.println(i);
+  Serial.print("\r\n");
+  delay(1000);
+}
+}
