afeb4bd4ec03498a468dde8c07da92bb91cc45a0
Victor Hugo Rodriguez Ledezma <stowny1500@gmail.com>
2025-10-29 20:58:14 -0600

    Primer commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace p5_SerialVisual
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.BtnConneted = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tbVoltaje = new System.Windows.Forms.TextBox();
+            this.tbValor = new System.Windows.Forms.TextBox();
+            this.BtnStop = new System.Windows.Forms.Button();
+            this.BtnPause = new System.Windows.Forms.Button();
+            this.BtnReset = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
+            // 
+            // BtnConneted
+            // 
+            this.BtnConneted.ForeColor = System.Drawing.Color.Red;
+            this.BtnConneted.Location = new System.Drawing.Point(431, 131);
+            this.BtnConneted.Name = "BtnConneted";
+            this.BtnConneted.Size = new System.Drawing.Size(75, 23);
+            this.BtnConneted.TabIndex = 3;
+            this.BtnConneted.Text = "Desconectado";
+            this.BtnConneted.UseVisualStyleBackColor = true;
+            this.BtnConneted.Click += new System.EventHandler(this.BtnConneted_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(501, 196);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 4;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(357, 199);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(131, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Seleccione Puerto Serial:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(387, 237);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(104, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Seleccione Baudios:";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(501, 234);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 6;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(67, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(200, 512);
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(362, 22);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Voltaje:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(362, 61);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(59, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Valor ADC:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(383, 275);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "label5";
+            // 
+            // tbVoltaje
+            // 
+            this.tbVoltaje.Location = new System.Drawing.Point(427, 19);
+            this.tbVoltaje.Name = "tbVoltaje";
+            this.tbVoltaje.Size = new System.Drawing.Size(100, 20);
+            this.tbVoltaje.TabIndex = 12;
+            // 
+            // tbValor
+            // 
+            this.tbValor.Location = new System.Drawing.Point(427, 54);
+            this.tbValor.Name = "tbValor";
+            this.tbValor.Size = new System.Drawing.Size(100, 20);
+            this.tbValor.TabIndex = 13;
+            // 
+            // BtnStop
+            // 
+            this.BtnStop.Location = new System.Drawing.Point(329, 417);
+            this.BtnStop.Name = "BtnStop";
+            this.BtnStop.Size = new System.Drawing.Size(75, 23);
+            this.BtnStop.TabIndex = 14;
+            this.BtnStop.Text = "Stop";
+            this.BtnStop.UseVisualStyleBackColor = true;
+            this.BtnStop.Click += new System.EventHandler(this.BtnStop_Click);
+            // 
+            // BtnPause
+            // 
+            this.BtnPause.Location = new System.Drawing.Point(469, 417);
+            this.BtnPause.Name = "BtnPause";
+            this.BtnPause.Size = new System.Drawing.Size(75, 23);
+            this.BtnPause.TabIndex = 15;
+            this.BtnPause.Text = "Pause";
+            this.BtnPause.UseVisualStyleBackColor = true;
+            this.BtnPause.Click += new System.EventHandler(this.BtnPause_Click);
+            // 
+            // BtnReset
+            // 
+            this.BtnReset.Location = new System.Drawing.Point(613, 417);
+            this.BtnReset.Name = "BtnReset";
+            this.BtnReset.Size = new System.Drawing.Size(75, 23);
+            this.BtnReset.TabIndex = 16;
+            this.BtnReset.Text = "Reset";
+            this.BtnReset.UseVisualStyleBackColor = true;
+            this.BtnReset.Click += new System.EventHandler(this.BtnReset_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 563);
+            this.Controls.Add(this.BtnReset);
+            this.Controls.Add(this.BtnPause);
+            this.Controls.Add(this.BtnStop);
+            this.Controls.Add(this.tbValor);
+            this.Controls.Add(this.tbVoltaje);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.BtnConneted);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Button BtnConneted;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox tbVoltaje;
+        private System.Windows.Forms.TextBox tbValor;
+        private System.Windows.Forms.Button BtnStop;
+        private System.Windows.Forms.Button BtnPause;
+        private System.Windows.Forms.Button BtnReset;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Drawing;
+using System.IO.Ports;
+using System.Text;
+using System.Windows.Forms;
+
+namespace p5_SerialVisual
+{
+    public partial class Form1 : Form
+    {
+        int[] buffer = new int[200];
+        static Bitmap mapaDeBits = new Bitmap(200, 1024);
+        Graphics graficElements = Graphics.FromImage(mapaDeBits);
+        Pen pluma = new Pen(Color.Lime, 2);
+
+        // Variables de control de estado
+        bool graficar = true; // Si es false, no se dibuja
+        bool pausa = false;   // Si es true, no se actualiza la gráfica ni el buffer
+
+        public Form1()
+        {
+            InitializeComponent();
+            CheckForIllegalCrossThreadCalls = false;
+
+            string[] puertos = SerialPort.GetPortNames();
+            comboBox1.Items.AddRange(puertos);
+
+            string[] baudios = { "57600", "78880", "115200" };
+            comboBox2.Items.AddRange(baudios);
+
+            // Opcional: seleccionar los primeros valores para evitar que estén vacíos
+            if (comboBox1.Items.Count > 0)
+                comboBox1.SelectedIndex = 0;
+
+            if (comboBox2.Items.Count > 0)
+                comboBox2.SelectedIndex = 0;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Aquí puedes agregar código que quieras ejecutar al cargar el formulario
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (serialPort1.IsOpen)
+                serialPort1.Close();
+        }
+
+        private void serialPort1_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            if (!graficar) return; // Si Stop está activo, no procesar nada
+
+            string cadena = serialPort1.ReadExisting();
+            string[] partes = cadena.Split(new[] { "\r\n" }, StringSplitOptions.RemoveEmptyEntries);
+            int valor = 0;
+
+            for (int ii = 0; ii < partes.Length; ii++)
+            {
+                string datoLimpio = partes[ii].Trim();
+
+                if (int.TryParse(datoLimpio, out valor))
+                {
+                    if (!pausa)
+                    {
+                        // Mover los valores del buffer a la izquierda
+                        for (int i = 0; i < buffer.Length - 1; i++)
+                            buffer[i] = buffer[i + 1];
+
+                        // Insertar nuevo valor al final del buffer (ajustado para que el gráfico quede dentro del bitmap)
+                        buffer[buffer.Length - 1] = (1023 - valor) / 2;
+                    }
+
+                    // Mostrar el valor numérico y voltaje usando Invoke para evitar errores de hilo
+                    tbValor.Invoke((MethodInvoker)(() => tbValor.Text = valor.ToString()));
+
+                    double voltaje = 3.3 * valor / 1023;
+                    tbVoltaje.Invoke((MethodInvoker)(() => tbVoltaje.Text = voltaje.ToString("0.00")));
+
+                    // Dibujar gráfico
+                    graficElements.Clear(Color.Black);
+
+                    for (int i = 0; i < buffer.Length - 1; i++)
+                        graficElements.DrawLine(pluma, i, buffer[i], i + 1, buffer[i + 1]);
+
+                    pictureBox1.Invoke((MethodInvoker)(() => pictureBox1.Image = mapaDeBits));
+                }
+            }
+        }
+
+        private void BtnConneted_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!serialPort1.IsOpen)
+                {
+                    serialPort1.Open();
+                    if (serialPort1.IsOpen)
+                    {
+                        BtnConneted.Text = "Conectado";
+                        BtnConneted.ForeColor = Color.Green;
+                    }
+                }
+                else
+                {
+                    serialPort1.Close();
+                    BtnConneted.Text = "Desconectado";
+                    BtnConneted.ForeColor = Color.Red;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al abrir el puerto serial, posiblemente está en uso por otro programa: " + ex.Message,
+                                "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedItem != null)
+                serialPort1.PortName = comboBox1.SelectedItem.ToString();
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox2.SelectedItem != null)
+            {
+                serialPort1.BaudRate = Convert.ToInt32(comboBox2.SelectedItem.ToString());
+                serialPort1.Encoding = Encoding.UTF8;
+            }
+        }
+
+        // Botón STOP: detiene completamente la lectura y graficado
+        private void BtnStop_Click(object sender, EventArgs e)
+        {
+            graficar = false;
+            pausa = false; // Desactivar pausa también
+            BtnStop.BackColor = Color.Red;
+            BtnPause.BackColor = SystemColors.Control;
+        }
+
+        // Botón PAUSE: congela la gráfica, pero sigue leyendo datos
+        private void BtnPause_Click(object sender, EventArgs e)
+        {
+            pausa = !pausa;
+            if (pausa)
+                BtnPause.BackColor = Color.Yellow;
+            else
+                BtnPause.BackColor = SystemColors.Control;
+        }
+
+        // Botón RESET: limpia el buffer y reinicia la gráfica
+        private void BtnReset_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < buffer.Length; i++)
+                buffer[i] = 0;
+
+            graficElements.Clear(Color.Black);
+            pictureBox1.Image = mapaDeBits;
+
+            tbValor.Text = "0";
+            tbVoltaje.Text = "0.00";
+
+            pausa = false;
+            graficar = true;
+
+            BtnPause.BackColor = SystemColors.Control;
+            BtnStop.BackColor = SystemColors.Control;
+        }
+    }
+}
+
